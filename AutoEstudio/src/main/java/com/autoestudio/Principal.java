0d59c1bc1307948e919d0265e94c934744395084
Administrador <Administrador@yasmin-PC>
2017-10-11 15:13:14 -0500

    commit inicial

---
+++ AutoEstudio/src/main/java/com/autoestudio/Principal.java
@@ -0,0 +1,14 @@
+package com.autoestudio;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Principal {
+	
+	private static final Logger logger = LogManager.getLogger();
+	
+//	public static void main(String[] args) {
+//		logger.error("Hola Mundo!");
+//	}
+
+}
